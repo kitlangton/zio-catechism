2707f80c12b8c7e72d53a11da8cc485e7c8d0b68
Kit Langton <kit.langton@gmail.com>
2020-08-07 18:00:08 -0700

    Update SignalSyntax
    Turn booleans into animated percentages

--- modules/frontend/src/main/scala/visualizations/SignalSyntax.scala
+++ modules/frontend/src/main/scala/visualizations/SignalSyntax.scala
@@ -1,10 +1,19 @@
 package visualizations
 
+import animator.{Animatable, Animator}
 import com.raquo.airstream.signal.Signal
 
 object SignalSyntax {
   implicit final class SignalOps[A](val value: Signal[A]) extends AnyVal {
-    def string: Signal[String]                           = value.map(_.toString)
+    def string: Signal[String]                                = value.map(_.toString)
+    def spring(implicit animatable: Animatable[A]): Signal[A] = Animator.spring(value)
+  }
+
+  implicit final class NumericSignalOps[A: Numeric](value: Signal[A]) {
     def px(implicit numeric: Numeric[A]): Signal[String] = value.map(d => s"${d}px")
+  }
+
+  implicit final class BooleanSignalOps(val value: Signal[Boolean]) extends AnyVal {
+    def percent: Signal[Double] = value.map(b => if (b) 1.0 else 0.0).spring
   }
 }
